4534fbac2b81253bc48535770a091f503ef22eec
Oscar Stiven Muñoz Ramirez <1083870123@u.icesi.edu.co>
2025-11-18 13:06:26 -0500

    Migrar de Tailwind CSS v4 a v3 para compatibilidad con Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ['lightningcss'],
-  experimental: {
-    // Asegura que los módulos nativos se copien correctamente en Vercel
-    outputFileTracingIncludes: {
-      '/': ['./node_modules/lightningcss/**/*'],
-    },
-  },
 };
 
 export default nextConfig;
